d17b4077d89a07a8e726eab5fea77eb56a4dffdb
rajahindustani1995 <rajeshdambavale959@gmail.com>
2022-06-10 00:45:19 +0530

    Starting Position of Player Added

--- SnakeandLadderSimulator/SnakeandLadderSimulator/Program.cs
+++ SnakeandLadderSimulator/SnakeandLadderSimulator/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace SnakeAndLadder
+﻿using SnakeandLadderSimulator;
+
+namespace SnakeAndLadder
 {
     class program
     {
         public static void Main(String[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Simulator Program");
+            SinglePlayerPosition position = new SinglePlayerPosition();
+            position.StartPosition();
         }
     }
 }
---
+++ SnakeandLadderSimulator/SnakeandLadderSimulator/SinglePlayerPosition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderSimulator
+{
+    internal class SinglePlayerPosition
+    {
+        public void StartPosition()
+        {
+            int playerPosition = 0;
+            Console.WriteLine("Starting Position Of the Player is =" + playerPosition);
+            
+        }
+    }
+}
